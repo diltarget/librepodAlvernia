13cc9e576eb8276741f1e610f8f518dc5d02d60c
Dylan <diltarget13@gmail.com>
2015-08-07 10:00:01 -0400

    Fixed the temperature library

--- libs/temp.js
+++ libs/temp.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 var dir = require('node-dir');
 var librepod = require('librepod');
 
-var dir = '/sys/bus/w1/devices/'
+var dirname = '/sys/bus/w1/devices/'
 
 exports.event = function(o,callback){
     if(o.interval == undefined) return
@@ -26,11 +26,15 @@ exports.event = function(o,callback){
     exec("sudo modprobe w1-gpio",function(){
         exec("sudo modprobe w1-therm",function(){
             setInterval(function(){
-                dir.subdirs(__dirname, function(err, subdir) {
+                dir.subdirs(dirname, function(err, subdir) {
                     if (err) throw err;
-                    if(subdir.substring(0,2) != "28") return; 
-                    readTemp(id,subdir,callback);
+
+		    Object.keys(subdir).forEach(function(k){
+                    	if(subdir[k].replace(dirname,"").substring(0,2) != "28") return; 
+                    	readTemp(id,subdir[k],callback);
+		    });
                 });
+		
             },parseInt(o.interval));
                	            
         });
@@ -40,7 +44,7 @@ exports.event = function(o,callback){
 }
 
 function readTemp(id, sub, callback){
-    fs.readFile(dir+'/'+sub+'/w1_slave','utf-8',function(err,html){
+    fs.readFile(sub+'/w1_slave','utf-8',function(err,html){
         if (err) throw err;
         var temp =  parseFloat(html.split("t=")[1])/1000.0;
         var header = sub.substring(3);
